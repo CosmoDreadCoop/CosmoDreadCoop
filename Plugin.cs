6f661b1baad8ed5fbde1f5e11f9bb09529c9584d
tontyGH <39193182+tontyGH@users.noreply.github.com>
2026-09-16 22:41:25 -0400

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace CosmoDreadCoop;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string PLUGIN_GUID = "org.tontygh.plugins.cosmodreadcoop";
+    public const string PLUGIN_NAME = "CosmoDread Co-Op";
+    public const string PLUGIN_VERSION = "0.0.1.0";
+
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
