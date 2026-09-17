15ef0ebf2fce50825fedfce30905fd28080b4974
tontyGH <39193182+tontyGH@users.noreply.github.com>
2026-09-16 23:38:35 -0400

    Implement global logger

---
+++ Logger.cs
@@ -0,0 +1,33 @@
+using BepInEx.Logging;
+
+namespace CosmoDreadCoop;
+
+internal static class CosmoLogger
+{
+	private static ManualLogSource logSource;
+
+	public static void SetSource(ManualLogSource logger)
+	{
+		logSource = logger;
+	}
+
+    public static void LogInfo(object message)
+    {
+        logSource.LogInfo(message);
+    }
+
+    public static void LogWarning(object message)
+    {
+        logSource.LogWarning(message);
+    }
+
+    public static void LogError(object message)
+    {
+        logSource.LogError(message);
+    }
+
+    public static void LogDebug(object message)
+    {
+        logSource.LogDebug(message);
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,4 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 
 namespace CosmoDreadCoop;
 
@@ -9,13 +8,11 @@ public class Plugin : BaseUnityPlugin
     public const string PLUGIN_GUID = "org.tontygh.plugins.cosmodreadcoop";
     public const string PLUGIN_NAME = "CosmoDread Co-Op";
     public const string PLUGIN_VERSION = "0.0.1.0";
-
-    internal static new ManualLogSource Logger;
         
     private void Awake()
     {
         // Plugin startup logic
-        Logger = base.Logger;
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        CosmoLogger.SetSource(Logger);
+        CosmoLogger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
     }
 }
