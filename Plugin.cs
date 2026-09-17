566e60af7b7302df6ad18075c2883fbc8b00f8bc
tontyGH <39193182+tontyGH@users.noreply.github.com>
2026-09-17 09:31:19 -0400

    Change logging message

--- Plugin.cs
+++ Plugin.cs
@@ -7,12 +7,12 @@ public class Plugin : BaseUnityPlugin
 {
     public const string PLUGIN_GUID = "org.tontygh.plugins.cosmodreadcoop";
     public const string PLUGIN_NAME = "CosmoDread Co-Op";
-    public const string PLUGIN_VERSION = "0.0.1.0";
+    public const string PLUGIN_VERSION = "0.1.0";
         
     private void Awake()
     {
         // Plugin startup logic
         CosmoLogger.SetSource(Logger);
-        CosmoLogger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        CosmoLogger.LogInfo($"Loaded {PLUGIN_GUID} v{PLUGIN_VERSION}");
     }
 }
